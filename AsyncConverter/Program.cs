e33816830f17ccaa38f30dcf3423a7f026cca48f
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2016-05-22 13:38:56 +0200

    Rearranged method

--- AsyncConverter/Program.cs
+++ AsyncConverter/Program.cs
@@ -108,76 +108,7 @@ namespace AsyncConverter
 		}
 	}
 
-	public static class Convert
-	{
-		public static string ToCodeString(this List<Variable> list, bool singleLine = false, bool trailingComma = false, bool startingComma = false, bool withValues = false, bool valuesOnly = false, bool namesOnly = false, bool setToThis = false)
-		{
-			if(setToThis || withValues)
-			{
-				singleLine = false;
-			}
-
-			string result = "";
-
-			if(startingComma)
-			{
-				result += ", ";
-			}
-
-			bool first = true;
-			foreach(Variable var in list)
-			{
-				if (first)
-				{
-					first = false;
-				}
-				else if(singleLine)
-				{
-					result += ", ";
-				}
-
-				if(!setToThis)
-				{
-					if(!withValues)
-					{
-						if(valuesOnly)
-						{
-							result += var.value;
-						}
-						else if(namesOnly)
-						{
-							result += var.name;
-						}
-						else
-						{
-							result += var.type + " " + var.name + ((!singleLine) ? ";" : "");
-						}
-					}
-					else
-					{
-						result += var.name + " = " + var.value + ";";
-					}
-				}
-				else
-				{
-					result += "this->" + var.name + " = " + var.name + ";";
-				}
-
-				if(!singleLine)
-				{
-					result += "\r\n";
-				}
-			}
-
-			if(trailingComma)
-			{
-				result += ", ";
-			}
-
-			return result;
-		}
-	}
-
+	
 	public class AwaitCall
 	{
 		// 0 = method type
@@ -301,5 +232,75 @@ namespace AsyncConverter
 		public string name;
 		public string type;
 		public string value;
+	}
+
+	public static class Convert
+	{
+		public static string ToCodeString(this List<Variable> list, bool singleLine = false, bool trailingComma = false, bool startingComma = false, bool withValues = false, bool valuesOnly = false, bool namesOnly = false, bool setToThis = false)
+		{
+			if (setToThis || withValues)
+			{
+				singleLine = false;
+			}
+
+			string result = "";
+
+			if (startingComma)
+			{
+				result += ", ";
+			}
+
+			bool first = true;
+			foreach (Variable var in list)
+			{
+				if (first)
+				{
+					first = false;
+				}
+				else if (singleLine)
+				{
+					result += ", ";
+				}
+
+				if (!setToThis)
+				{
+					if (!withValues)
+					{
+						if (valuesOnly)
+						{
+							result += var.value;
+						}
+						else if (namesOnly)
+						{
+							result += var.name;
+						}
+						else
+						{
+							result += var.type + " " + var.name + ((!singleLine) ? ";" : "");
+						}
+					}
+					else
+					{
+						result += var.name + " = " + var.value + ";";
+					}
+				}
+				else
+				{
+					result += "this->" + var.name + " = " + var.name + ";";
+				}
+
+				if (!singleLine)
+				{
+					result += "\r\n";
+				}
+			}
+
+			if (trailingComma)
+			{
+				result += ", ";
+			}
+
+			return result;
+		}
 	}
 }
